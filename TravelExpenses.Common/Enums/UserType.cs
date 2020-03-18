a342fb94cb93ed1f055dfa453fdaf176feaf98c3
jemma <jemmanuelmg@hotmail.com>
2020-03-17 20:01:10 -0500

    Created UserEntity class  and UserType Enum. Created IUserHelper interface

---
+++ TravelExpenses.Common/Enums/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelExpenses.Common.Enums
+{
+    public enum UserType
+    {
+        Admin,
+        User
+
+    }
+}
---
+++ TravelExpenses.Web/Data/Entities/UserEntity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using TravelExpenses.Common.Enums;
+
+namespace TravelExpenses.Web.Data.Entities
+{
+    public class UserEntity : IdentityUser
+    {
+        [Display(Name = "Document")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Picture")]
+        public string PicturePath { get; set; }
+
+        [Display(Name = "User Type")]
+        public UserType UserType { get; set; }
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+
+    }
+}
